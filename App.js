aa1ab269e01140bd47863892c909fecefc7e88c1
ShazaHafeez <shazal.masood@gmail.com>
2023-06-16 11:28:48 +0500

    init

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   console.log("App Executed")
-  let x;
-  x.toString();
   return (
     <View style={styles.container}>
       <Text>Welcome to My First Andriod App</Text>
