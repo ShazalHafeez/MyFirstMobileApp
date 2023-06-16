63f1a9a8fa2bd9c07c3aa195be86c1a66f27ce2b
ShazaHafeez <shazal.masood@gmail.com>
2023-06-16 11:36:17 +0500

    test change

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  console.log("App Executed")
   return (
     <View style={styles.container}>
       <Text>Welcome to My First Andriod App</Text>
