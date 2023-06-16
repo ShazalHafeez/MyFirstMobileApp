4a1ce301f69271048d5ac8145f04121eafb0ab26
ShazaHafeez <shazal.masood@gmail.com>
2023-06-16 11:21:19 +0500

    initial changes

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  console.log("App Executed")
+  let x;
+  x.toString();
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to My First Andriod App</Text>
+      <Text>How can we help you!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffb6c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
